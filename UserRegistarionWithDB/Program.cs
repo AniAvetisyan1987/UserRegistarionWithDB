6ccee6f3d652450e228606efaffa4b4f6dd3349c
Armen <Armen@DESKTOP-IRSKKA4>
2023-07-25 14:23:58 +0400

    Add project files.

---
+++ UserRegistarionWithDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using UserRegistarionWithDB.Users;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        AddUser(1, "Anna", "AnnaAvetyan", "123456");
+        AddUser(2, "Artur", "ArturAvetyan", "123456");
+        AddUser(3, "Armine", "ArmineAvetyan", "123456");
+        AddUser(4, "Anna", "AnnaAvetyan", "234167");
+    }
+
+    static void AddUser(int id, string name, string login, string pass)
+    {
+        var context = new UsersContext();
+        var user = new User { Id = id, Username = name, Login = login, Password = pass };
+        context.Add(user);
+        context.SaveChanges();
+    }
+}
